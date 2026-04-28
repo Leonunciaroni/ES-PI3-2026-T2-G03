cd7d467dc7675e5ceebd9c1513b0db7602a284e4
Pedro Henrique Contardi Soler <pedrocsoler@hotmail.com>
2026-04-27 22:15:50 -0300

    logo do app bar conforme claro/escuro

--- lib/carteira/widgets/carteira_app_bar_logo.dart
+++ lib/carteira/widgets/carteira_app_bar_logo.dart
@@ -5,26 +5,23 @@
 
 import 'package:flutter/material.dart';
 
-import '../../theme/app_colors.dart';
+import '../../theme/mescla_brand_logo.dart';
 
 /// Logo da marca, centrado no espaço do título da [AppBar].
 class CarteiraAppBarLogo extends StatelessWidget {
   const CarteiraAppBarLogo({super.key});
 
   static const _logoHeight = 48.0;
+  static const _logoBoxWidth = 200.0;
 
   @override
   Widget build(BuildContext context) {
     return FittedBox(
       fit: BoxFit.scaleDown,
       alignment: Alignment.center,
-      child: Image.asset(
-        AppColors.mesclaLogoAsset,
-        height: _logoHeight,
-        fit: BoxFit.contain,
-        errorBuilder: (context, error, stackTrace) {
-          return const SizedBox.shrink();
-        },
+      child: MesclaBrandLogo(
+        boxWidth: _logoBoxWidth,
+        boxHeight: _logoHeight,
       ),
     );
   }
