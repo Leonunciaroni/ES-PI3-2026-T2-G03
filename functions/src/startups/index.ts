399193d9634bbfda88c2111eb6ffdb642aac3724
Pedro Henrique Contardi Soler <pedrocsoler@hotmail.com>
2026-05-07 16:20:07 -0300

    functions(startups): export reconcileStartupCapture

--- functions/src/startups/index.ts
+++ functions/src/startups/index.ts
@@ -4,3 +4,4 @@
 export {listStartups} from "./handlers/listStartups.js";
 export {getStartupDetails} from "./handlers/getStartupDetails.js";
 export {createStartupQuestion} from "./handlers/createStartupQuestion.js";
+export {reconcileStartupCapture} from "./handlers/reconcileStartupCapture.js";
