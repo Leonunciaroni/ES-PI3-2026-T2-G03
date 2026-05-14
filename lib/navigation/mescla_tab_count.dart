847df95f7ea21bde8f3f8b1c58b1488012cea7f0
Pedro Henrique Contardi Soler <pedrocsoler@hotmail.com>
2026-05-14 14:15:32 -0300

    feat(navegação): adicionar kMesclaMainTabCount como constante partilhada

---
+++ lib/navigation/mescla_tab_count.dart
@@ -0,0 +1,8 @@
+// Autor principal: Pedro Henrique Contardi Soler
+// RA: 25005592
+//
+// Constante partilhada pelo shell e por ecrãs que validam o índice da barra inferior,
+// sem acoplar serviços de auth a widgets.
+
+/// Início, Carteira, Balcão, Catálogo, Perfil.
+const int kMesclaMainTabCount = 5;
